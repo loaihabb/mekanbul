60a1e44722aa7d9b5f151d8dcbffbfa3c5139f43
LoaiHabb <loaihabb@gmail.com>
2022-11-10 18:45:31 +0300

    ders4

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -3,6 +3,13 @@ var mongoose=require("mongoose");
 var dbURI="mongodb://Loiy:12345@ac-cramdwd-shard-00-00.qzrthyd.mongodb.net:27017,ac-cramdwd-shard-00-01.qzrthyd.mongodb.net:27017,ac-cramdwd-shard-00-02.qzrthyd.mongodb.net:27017/?ssl=true&replicaSet=atlas-25gfs4-shard-0&authSource=admin&retryWrites=true&w=majority";
 mongoose.connect(dbURI);
 
+function kapat(msg,callback){
+    mongoose.connection.close(function(){
+        console.log(msg);
+        callback();
+    });
+}
+
 process.on("SIGINT",function(){
     kapat("Uygulama kapatıldı",function(){
         process.exit(0);
