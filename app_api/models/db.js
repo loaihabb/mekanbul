5bc51fd4a21723c549de93ebee5f0af61d3d65bd
LoaiHabb <loaihabb@gmail.com>
2022-11-20 18:28:30 +0300

    Thunder

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,6 +1,8 @@
 var mongoose=require("mongoose");
+require("./mekansema");
 //var dbURI = 'mongodb://localhost/mekanbul'; 
-var dbURI="mongodb://Loiy:12345@ac-cramdwd-shard-00-00.qzrthyd.mongodb.net:27017,ac-cramdwd-shard-00-01.qzrthyd.mongodb.net:27017,ac-cramdwd-shard-00-02.qzrthyd.mongodb.net:27017/?ssl=true&replicaSet=atlas-25gfs4-shard-0&authSource=admin&retryWrites=true&w=majority";
+//var dbURI="mongodb://Loiy:12345@ac-cramdwd-shard-00-00.qzrthyd.mongodb.net:27017,ac-cramdwd-shard-00-01.qzrthyd.mongodb.net:27017,ac-cramdwd-shard-00-02.qzrthyd.mongodb.net:27017/?ssl=true&replicaSet=atlas-25gfs4-shard-0&authSource=admin&retryWrites=true&w=majority";
+var dbURI="mongodb+srv://Loiy:12345@mekanbul.qzrthyd.mongodb.net/mekanbul?retryWrites=true&w=majority"; 
 mongoose.connect(dbURI);
 
 function kapat(msg,callback){
@@ -32,4 +34,3 @@ mongoose.connection.on("connected",function(){
 })
 
 
-require("./mekansema")
