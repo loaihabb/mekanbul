a6af3ef62f9575348be3a6c0d27f201fd2ea0a2e
LoaiHabb <loaihabb@gmail.com>
2022-12-08 13:54:36 +0300

    Ödev8

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,8 +1,8 @@
 var mongoose=require("mongoose");
 require("./mekansema");
-//var dbURI = 'mongodb://localhost/mekanbul'; 
+var dbURI = 'mongodb://localhost/mekanbul'; 
 //var dbURI="mongodb://Loiy:12345@ac-cramdwd-shard-00-00.qzrthyd.mongodb.net:27017,ac-cramdwd-shard-00-01.qzrthyd.mongodb.net:27017,ac-cramdwd-shard-00-02.qzrthyd.mongodb.net:27017/?ssl=true&replicaSet=atlas-25gfs4-shard-0&authSource=admin&retryWrites=true&w=majority";
-var dbURI="mongodb+srv://Loiy:1234@mekanbul.jqn65ae.mongodb.net/?retryWrites=true&w=majority"; 
+//var dbURI="mongodb+srv://Loiy:1234@mekanbul.jqn65ae.mongodb.net/?retryWrites=true&w=majority"; 
 mongoose.connect(dbURI);
 
 function kapat(msg,callback){
